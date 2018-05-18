d46237a40ceb226e696426c1016bac0ce431773c
Joaquin Umpierrez <jo.umpierrez@gmail.com>
2018-05-18 11:24:09 -0300

    Update PreparacionParcial.sql

--- PreparacionParcial.sql
+++ PreparacionParcial.sql
@@ -3,11 +3,14 @@ GO;
 
 USE BaseDeDatos2_PreparacionParcial;
 CREATE TABLE Montura (IdMontura int CONSTRAINT PK_Montura PRIMARY KEY, Marca varchar(20), Precio int, Nombre varchar(20));
+                                                                                                                     
 CREATE TABLE Color (IdColor int CONSTRAINT PK_Color PRIMARY KEY, DescripcionColor varchar(20));
+                                                                                          
 CREATE TABLE Stock (IdMontura int, IdColor int, Cantidad int,
 CONSTRAINT PK_Stock PRIMARY KEY (IdMontura, IdColor),
 CONSTRAINT FK_Stock_Montura FOREIGN KEY (IdMontura) REFERENCES Montura(IdMontura),
 CONSTRAINT FK_Stock_Color FOREIGN KEY (IdColor) REFERENCES Color(IdColor));
+                                                                 
 CREATE TABLE Venta (Fecha date, IdMontura int, IdColor int, CantVendido int, Precio int,
 CONSTRAINT PK_Venta PRIMARY KEY (Fecha, IdMontura, IdColor),
 CONSTRAINT FK_Venta_Montura FOREIGN KEY (IdMontura) REFERENCES Montura(IdMontura),
@@ -16,10 +19,10 @@ CONSTRAINT FK_Venta_Color FOREIGN KEY (IdColor) REFERENCES Color(IdColor));
 -- 1) Precio montura entre 1.000 y 100.000
 ALTER TABLE Montura ADD CHECK (Precio >= 1000 AND Precio <= 100000)
 
--- 2) Crear Indices sobre FKs
+-- 2) Crear Indices sobre FKs ||//No se que hacer aca\\||
 
 -- 3) Realizar Modificaciones Utilizando PROC o FUNC
--- 3.a) Borrar las monturas que valgan menos de x pesos y donde el nombre contenga ‘error’.
+-- 3.a) Borrar las monturas que valgan menos de x pesos y donde el nombre contenga â€˜errorâ€™.
 CREATE PROCEDURE BorrarMonturas
 @valorAborrar int
  AS
@@ -27,7 +30,7 @@ BEGIN
 DELETE FROM Montura WHERE Precio < @valorAborrar AND Nombre = 'error';
 END
 
--- 3.b) Modificar el precio total de las ventas realizadas un determinado día. Se necesita decrementar el mismo en un 10%.
+-- 3.b) Modificar el precio total de las ventas realizadas un determinado dÃ­a. Se necesita decrementar el mismo en un 10%.
 CREATE PROCEDURE ModificarPrecioVentas
 @dia date
 AS
@@ -35,7 +38,7 @@ BEGIN
 UPDATE Venta SET Precio = Precio*0.9 WHERE Fecha = @dia;
 END
 
--- 3.c) Borrar una determinada montura y los datos asociados a ésta.
+-- 3.c) Borrar una determinada montura y los datos asociados a Ã©sta.
 CREATE PROCEDURE BorrarMontura
 @montura int
 AS
@@ -47,8 +50,8 @@ END
 
 -- 4) Resolver las siguientes consultas SQL utilizando vistas, procedimientos o funciones
 
--- 4.a)	Se necesita tener un listado de las monturas con un determinado nombre que valgan más de 10.000 pesos, 
---		en donde dichas monturas se hayan vendido en un determinado día o en otro día.
+-- 4.a)	Se necesita tener un listado de las monturas con un determinado nombre que valgan mÃ¡s de 10.000 pesos, 
+--		en donde dichas monturas se hayan vendido en un determinado dÃ­a o en otro dÃ­a.
 CREATE VIEW [MonturasCarasVendidas] AS
 SELECT * FROM Montura WHERE EXISTS
 (SELECT * FROM Venta WHERE IdMontura = Montura.IdMontura);
@@ -60,18 +63,18 @@ SELECT * FROM Montura WHERE EXISTS
 GROUP BY  (IdMontura)
 HAVING COUNT (IdColor) = 2);
 
--- 4.c)	Seleccionar la combinación montura-color donde dicha combinación NO se haya vendido nunca en la óptica.
+-- 4.c)	Seleccionar la combinaciÃ³n montura-color donde dicha combinaciÃ³n NO se haya vendido nunca en la Ã³ptica.
 CREATE VIEW NoVendida AS
 SELECT * FROM Stock WHERE NOT EXISTS
 (SELECT * FROM Venta WHERE IdMontura = Stock.IdMontura AND IdColor = Stock.IdColor);
 
--- 4.d)	Interesa tener un detalle de los últimos 5 días. 
+-- 4.d)	Interesa tener un detalle de los Ãºltimos 5 dÃ­as. 
 --		1.	Fecha de venta
 --		2.	Cantidad vendido
 --		3.	Precio de la venta 
 --		4.	Nombre de la montura
 --		5.	Marca de la montura
---		6.	Descripción del color de la montura. 
+--		6.	DescripciÃ³n del color de la montura. 
 --		Ordenar la salida por fecha y montura.
 
 CREATE VIEW [Ventas5Dias]
